bc065c457b6118cccfdfc52425f2966216074c7e
dav100ide <oreglio.dev@gmail.com>
2022-10-20 14:32:09 +0200

    prezzo 2 decimali

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 let kilometers = Number(window.prompt('la distanza del tuo viaggio in kilometri?'));
 let userAge = Number(window.prompt('quanti anni hai?'));
 let ticketPrice = kilometers * 0.21; // il prezzo base è 0.21€ al kilometro
-
+let discountName = `spiacenti non è stato applicato nessuno sconto`;
 // se l'utente ha meno di 18 anni c'è lo sconto del 20%
 if (userAge < 18) {
    ticketPrice -= ticketPrice * 0.2;
-   console.log(ticketPrice);
+   discountName = `è stato applicato lo sconto minorenni`;
 }
 // se l'utente è un vecchio over 65 (65compreso) c'è lo sconto del 40%
 else if (userAge >= 65) {
    ticketPrice -= ticketPrice * 0.4;
-   console.log(`prezzo anziano ${ticketPrice}`);
+   discountName = `è stato applicato lo sconto anziani`;
 }
-
-document.getElementById('final-price').innerHTML = `il bigletto costa €${ticketPrice}`;
+// arrontondo a due decimali
+document.getElementById('final-price').innerHTML = `il bigletto costa €
+${ticketPrice.toLocaleString('en-US', { maximumFractionDigits: 2, minimumFractionDigits: 2 })} ${discountName}`;
